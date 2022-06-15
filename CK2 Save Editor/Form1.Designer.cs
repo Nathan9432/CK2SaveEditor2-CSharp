1eebf61ce946960d7ba9c9ced447a433ec6c437c
Coolguybest <njboertje@gmail.com>
2022-06-14 19:56:12 -0500

    First true commit. Trying out VS Git.

--- CK2 Save Editor/Form1.Designer.cs
+++ CK2 Save Editor/Form1.Designer.cs
@@ -34,22 +34,37 @@ namespace CK2_Save_Editor
 			this.lblStewardship = new System.Windows.Forms.Label();
 			this.lblIntrigue = new System.Windows.Forms.Label();
 			this.lblLearning = new System.Windows.Forms.Label();
+			this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+			this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+			this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+			this.numericUpDown4 = new System.Windows.Forms.NumericUpDown();
+			this.numericUpDown5 = new System.Windows.Forms.NumericUpDown();
+			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown5)).BeginInit();
+			this.tableLayoutPanel1.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// lblDiplomacy
 			// 
+			this.lblDiplomacy.Anchor = System.Windows.Forms.AnchorStyles.None;
 			this.lblDiplomacy.AutoSize = true;
-			this.lblDiplomacy.Location = new System.Drawing.Point(22, 45);
+			this.lblDiplomacy.Location = new System.Drawing.Point(7, 0);
 			this.lblDiplomacy.Name = "lblDiplomacy";
 			this.lblDiplomacy.Size = new System.Drawing.Size(56, 13);
 			this.lblDiplomacy.TabIndex = 0;
 			this.lblDiplomacy.Text = "Diplomacy";
+			this.lblDiplomacy.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
 			this.lblDiplomacy.Click += new System.EventHandler(this.label1_Click);
 			// 
 			// lblMartial
 			// 
+			this.lblMartial.Anchor = System.Windows.Forms.AnchorStyles.None;
 			this.lblMartial.AutoSize = true;
-			this.lblMartial.Location = new System.Drawing.Point(94, 45);
+			this.lblMartial.Location = new System.Drawing.Point(87, 0);
 			this.lblMartial.Name = "lblMartial";
 			this.lblMartial.Size = new System.Drawing.Size(38, 13);
 			this.lblMartial.TabIndex = 1;
@@ -57,8 +72,9 @@ namespace CK2_Save_Editor
 			// 
 			// lblStewardship
 			// 
+			this.lblStewardship.Anchor = System.Windows.Forms.AnchorStyles.None;
 			this.lblStewardship.AutoSize = true;
-			this.lblStewardship.Location = new System.Drawing.Point(149, 45);
+			this.lblStewardship.Location = new System.Drawing.Point(145, 0);
 			this.lblStewardship.Name = "lblStewardship";
 			this.lblStewardship.Size = new System.Drawing.Size(65, 13);
 			this.lblStewardship.TabIndex = 2;
@@ -66,8 +82,9 @@ namespace CK2_Save_Editor
 			// 
 			// lblIntrigue
 			// 
+			this.lblIntrigue.Anchor = System.Windows.Forms.AnchorStyles.None;
 			this.lblIntrigue.AutoSize = true;
-			this.lblIntrigue.Location = new System.Drawing.Point(234, 45);
+			this.lblIntrigue.Location = new System.Drawing.Point(227, 0);
 			this.lblIntrigue.Name = "lblIntrigue";
 			this.lblIntrigue.Size = new System.Drawing.Size(42, 13);
 			this.lblIntrigue.TabIndex = 3;
@@ -75,27 +92,96 @@ namespace CK2_Save_Editor
 			// 
 			// lblLearning
 			// 
+			this.lblLearning.Anchor = System.Windows.Forms.AnchorStyles.None;
 			this.lblLearning.AutoSize = true;
-			this.lblLearning.Location = new System.Drawing.Point(307, 45);
+			this.lblLearning.Location = new System.Drawing.Point(296, 0);
 			this.lblLearning.Name = "lblLearning";
 			this.lblLearning.Size = new System.Drawing.Size(48, 13);
 			this.lblLearning.TabIndex = 4;
 			this.lblLearning.Text = "Learning";
+			// 
+			// numericUpDown1
+			// 
+			this.numericUpDown1.Anchor = System.Windows.Forms.AnchorStyles.None;
+			this.numericUpDown1.Location = new System.Drawing.Point(5, 17);
+			this.numericUpDown1.Name = "numericUpDown1";
+			this.numericUpDown1.Size = new System.Drawing.Size(60, 20);
+			this.numericUpDown1.TabIndex = 5;
+			// 
+			// numericUpDown2
+			// 
+			this.numericUpDown2.Anchor = System.Windows.Forms.AnchorStyles.None;
+			this.numericUpDown2.Location = new System.Drawing.Point(76, 17);
+			this.numericUpDown2.Name = "numericUpDown2";
+			this.numericUpDown2.Size = new System.Drawing.Size(60, 20);
+			this.numericUpDown2.TabIndex = 6;
+			// 
+			// numericUpDown3
+			// 
+			this.numericUpDown3.Anchor = System.Windows.Forms.AnchorStyles.None;
+			this.numericUpDown3.Location = new System.Drawing.Point(147, 17);
+			this.numericUpDown3.Name = "numericUpDown3";
+			this.numericUpDown3.Size = new System.Drawing.Size(60, 20);
+			this.numericUpDown3.TabIndex = 7;
+			// 
+			// numericUpDown4
+			// 
+			this.numericUpDown4.Anchor = System.Windows.Forms.AnchorStyles.None;
+			this.numericUpDown4.Location = new System.Drawing.Point(218, 17);
+			this.numericUpDown4.Name = "numericUpDown4";
+			this.numericUpDown4.Size = new System.Drawing.Size(60, 20);
+			this.numericUpDown4.TabIndex = 8;
+			// 
+			// numericUpDown5
+			// 
+			this.numericUpDown5.Anchor = System.Windows.Forms.AnchorStyles.None;
+			this.numericUpDown5.Location = new System.Drawing.Point(290, 17);
+			this.numericUpDown5.Name = "numericUpDown5";
+			this.numericUpDown5.Size = new System.Drawing.Size(60, 20);
+			this.numericUpDown5.TabIndex = 9;
+			// 
+			// tableLayoutPanel1
+			// 
+			this.tableLayoutPanel1.ColumnCount = 5;
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+			this.tableLayoutPanel1.Controls.Add(this.numericUpDown1, 0, 1);
+			this.tableLayoutPanel1.Controls.Add(this.lblMartial, 1, 0);
+			this.tableLayoutPanel1.Controls.Add(this.lblStewardship, 2, 0);
+			this.tableLayoutPanel1.Controls.Add(this.lblIntrigue, 3, 0);
+			this.tableLayoutPanel1.Controls.Add(this.lblLearning, 4, 0);
+			this.tableLayoutPanel1.Controls.Add(this.numericUpDown5, 4, 1);
+			this.tableLayoutPanel1.Controls.Add(this.lblDiplomacy, 0, 0);
+			this.tableLayoutPanel1.Controls.Add(this.numericUpDown4, 3, 1);
+			this.tableLayoutPanel1.Controls.Add(this.numericUpDown2, 1, 1);
+			this.tableLayoutPanel1.Controls.Add(this.numericUpDown3, 2, 1);
+			this.tableLayoutPanel1.Location = new System.Drawing.Point(18, 115);
+			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+			this.tableLayoutPanel1.RowCount = 2;
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(357, 42);
+			this.tableLayoutPanel1.TabIndex = 10;
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Controls.Add(this.lblLearning);
-			this.Controls.Add(this.lblIntrigue);
-			this.Controls.Add(this.lblStewardship);
-			this.Controls.Add(this.lblMartial);
-			this.Controls.Add(this.lblDiplomacy);
+			this.Controls.Add(this.tableLayoutPanel1);
 			this.Name = "Form1";
 			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown5)).EndInit();
+			this.tableLayoutPanel1.ResumeLayout(false);
+			this.tableLayoutPanel1.PerformLayout();
 			this.ResumeLayout(false);
-			this.PerformLayout();
 
 		}
 
@@ -106,6 +192,12 @@ namespace CK2_Save_Editor
 		private System.Windows.Forms.Label lblStewardship;
 		private System.Windows.Forms.Label lblIntrigue;
 		private System.Windows.Forms.Label lblLearning;
+		private System.Windows.Forms.NumericUpDown numericUpDown1;
+		private System.Windows.Forms.NumericUpDown numericUpDown2;
+		private System.Windows.Forms.NumericUpDown numericUpDown3;
+		private System.Windows.Forms.NumericUpDown numericUpDown4;
+		private System.Windows.Forms.NumericUpDown numericUpDown5;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
 	}
 }
 
